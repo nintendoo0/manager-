aa1e78c90233a061bda73d79013a3a2d30a8c830
Arseniy <arseniyb.2005@gmail.com>
2025-10-08 04:28:28 +0300

    ы

--- frontend/src/components/Projects/ProjectList.js
+++ frontend/src/components/Projects/ProjectList.js
@@ -31,7 +31,7 @@ const ProjectList = () => {
   }, []);
 
   function openProject(projectId) {
-    // переходит на страницу проекта; убедитесь, что маршрут /projects/:id настроен
+    // переходит на страницу проекта; убедитесь, что маршрут /projects/:id настрое
     navigate(`/projects/${projectId}`);
   }
 
